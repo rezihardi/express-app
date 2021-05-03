3b311ffd1f2c753a13816d1709d909048ff7b315
Shiftwell <rezydoerasta@gmail.com>
2021-05-03 21:45:09 +0700

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+// import page controller
+import { Home, About, Contact } from "../controller/page.js";
+ 
+// init express router
+const router = express.Router();
+ 
+// Home route
+router.get('/', Home);
+ 
+// About route
+router.get('/about', About);
+ 
+// Contact route
+router.get('/contact', Contact);
+ 
+// export default router
+export default router;
